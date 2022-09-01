10aa1f5ec88e6483499e35391f21fbde99e3a9b1
Ingrid Aquino <ingridaquinosoares@live.com>
2022-09-01 20:59:40 -0300

    :construction: Criado a funcao upadatedModel

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,11 +1,33 @@
 import { Button, Grid, TextField, Typography } from "@material-ui/core";
 import { Box } from "@mui/material";
 
+import { ChangeEvent, useState } from "react";
 import { Link } from "react-router-dom";
 
+import UserLogin from "../../models/UserLogin";
 import "./Login.css";
 
 function Login() {
+
+  // irá armazenar os usuarios logado
+    const [userLogin, setUserLogin] = useState<UserLogin>({
+        id: 0,
+        usuario: '',
+        senha: '',
+        token: ''
+    })
+
+    //  function que ficará de olho se houve alguma mudança, e irar capturar o novo valor inserido. Sendo assim atualizando o login
+      function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+        setUserLogin({
+            ...userLogin,
+            [e.target.name] : e.target.value
+        })
+      }
+
+
+
+
   return (
      <Grid container direction="row" justifyContent="center" alignItems="center"> 
         <Grid xs={6} className='grid-image'></Grid>
@@ -14,8 +36,9 @@ function Login() {
           <Box paddingX={20}>
             <form>
                 <Typography variant="h3" gutterBottom component="h3" className="text-enter"> Entrar </Typography>
-                <TextField  id="usuario" label="usuário" variant="outlined" name="usuario" margin="normal" fullWidth/>
-                <TextField  id="senha" label="senha" variant="outlined" name="senha" margin="normal" type='password' fullWidth/>
+                      {/* VALUE={userLogin.usuario} (como se estivesse vinculando o model com o input) e no onchange  */}
+                <TextField  value={userLogin.usuario} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id="usuario" label="usuário" variant="outlined" name="usuario" margin="normal" fullWidth/>
+                <TextField  value={userLogin.senha} onChange={(e:ChangeEvent<HTMLInputElement>)=> updatedModel(e)} id="senha" label="senha" variant="outlined" name="senha" margin="normal" type='password'                                                                 fullWidth/>
                 <Box marginTop={2} textAlign='center'>
                   <Link to='/home' className="text-decorator-name">
                     <Button type="submit" variant="contained" className="button-login color-text"> 
