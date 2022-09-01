6e0e4647a08f2062cfe39c0a6fa43001414bfd19
Ingrid Aquino <ingridaquinosoares@live.com>
2022-09-01 19:39:53 -0300

    :construction: Criado a regra de negocio do services com axios

--- src/services/Services.ts
+++ src/services/Services.ts
@@ -7,6 +7,11 @@ export const api = axios.create({
     baseURL: 'https://blog2-indy.herokuapp.com/' //base(unica) url, url do meu backend do blog
 })
 
-export const login = async(url: any, dados: any, setDados: any) {
-  
+
+//url seria o /usuario/login
+//dados os dados do usuario na interface
+
+export const login = async(url: any, dados: any, setDados: any) => {
+    const resposta = await api.post(url, dados)
+    setDados(resposta.data.token)
 }
