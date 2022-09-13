487bfdd308af37cc8dbee8525d37fd808caac448
Ingrid Aquino <ingridaquinosoares@live.com>
2022-09-13 10:08:46 -0300

    :construction: Toastify implemetado no app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,17 @@ import CadastroPost from "./components/postagens/cadastroPost/CadastroPost";
 import CadastroTema from "./components/temas/cadastroTema/CadastrarTema";
 import DeletarPost from "./components/postagens/deletarPost/DeletarPost";
 import DeletarTema from "./components/temas/deletarTema/DeletarTema";
+
 import { Provider } from "react-redux";
 import store from "./store/store";
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <Provider store={store}>
+      <ToastContainer />
       <Router>
         <Navbar />
 
